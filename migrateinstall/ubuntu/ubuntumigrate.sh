ad55710ec18d790566476c7ddf69e0bb2e092057
LPulle <lpulle@nhs>
2019-05-04 12:12:16 +0100

    Added ubuntumigrate and readme

---
+++ migrateinstall/ubuntu/ubuntumigrate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# install svn
+apt install subversion
+
+# download the files
+# make sure you don't have a folder called "files"
+svn export https://github.com/LPulle/Linux/trunk/migrateinstall/ubuntu/files
+
+# Update base install, install latest dist and clean
+sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+
+# Copy repo keys and apt sources
+sudo apt-key add ~/files/Repo.keys
+sudo apt-key update
+sudo cp -R ~/files/sources.list* /etc/apt/
+
+# Install packages with apt-get and dpkg
+sudo apt-get update
+sudo apt-get install dselect
+sudo dselect update
+apt-cache dumpavail > ~/temp_avail
+sudo dpkg --merge-avail ~/temp_avail
+rm ~/temp_avail
+sudo dpkg --clear-selections
+sudo dpkg --set-selections < ~/files/Package.list
+sudo apt-get dselect-upgrade -y
