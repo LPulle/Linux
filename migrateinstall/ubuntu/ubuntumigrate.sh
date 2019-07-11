56fe67cbd7d2c98e18b6fcb9ebf6b55755307240
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2019-07-11 14:12:15 +0100

    Update ubuntumigrate.sh
    Amended to use the additions/amendments in the centos script

--- migrateinstall/ubuntu/ubuntumigrate.sh
+++ migrateinstall/ubuntu/ubuntumigrate.sh
@@ -9,7 +9,7 @@ sudo apt -y install subversion
 # download the files
 # make sure you don't have a folder called "files"
 svn export https://github.com/LPulle/Linux/trunk/migrateinstall/ubuntu/files
-cp -R ~/GitHub/Linux/migrateinstall/ubuntu/files/* ~/files
+yes | sudo /bin/cp -fR ~/GitHub/Linux/migrateinstall/ubuntu/files/* ~/files
 
 # Update base install, install latest dist and clean
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
@@ -34,15 +34,25 @@ sudo apt-get dselect-upgrade -y
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 
 # Some software manages upgrades with their own package manager
-#Python pip
-sudo -H pip install --upgrade pip
+#Python pip - amend the commands if your python/pip are called something different
+#2.7
+sudo -H pip2 install --upgrade pip
+sudo -H pip2 install --upgrade setuptools pip
+sudo -H python2 -m pip install --upgrade pip
+#3.6
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install --upgrade setuptools pip
-sudo -H python -m pip install --upgrade pip
+sudo -H python3 -m pip install --upgrade pip
 
 # Install python modules exported
-sudo -H pip2 install -r ~/files/pip2freeze.txt
-sudo -H pip3 install -r ~/files/pip3freeze.txt
+# this should work but it stops if it gets an error:
+#sudo -H pip2 install -r ~/files/pip2freeze.txt
+#sudo -H pip3 install -r ~/files/pip3freeze.txt
+
+# install using a for loop so will keep going even if it encounters an error
+# it will find errors as pipfreezeinstall.txt is a merge of python 2 and 3 module exports
+for i in $(cat ~/files/pipfreezeinstall.txt); do sudo /usr/local/bin/pip3.7 -qq install $i; done
+for i in $(cat ~/files/pipfreezeinstall.txt); do sudo pip3 -qq install $i; done
 
 # Install npm modules exported
 sed 's/#.*//' npmmodules.txt | xargs sudo npm install -g # this should work but stops if it gets an error
