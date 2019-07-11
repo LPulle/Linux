3360a6ecaf62214ac82ce2d4ad0ac363756617c7
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2019-07-11 14:00:59 +0100

    Update ubuntuexport.sh
    Amended to use the additions/amendments in the centos script

--- migrateinstall/ubuntu/ubuntuexport.sh
+++ migrateinstall/ubuntu/ubuntuexport.sh
@@ -4,6 +4,15 @@ sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 
 # update Linux repo with latest files
 cd ~/GitHub/Linux
+if [ ! -d "$DIRECTORY" ]; then
+  # If GitHub repo folder is missing clone it
+  cd ~/GitHub
+  git clone https://github.com/LPulle/Linux
+  cd Linux
+  else
+   cd ~/GitHub/Linux 
+fi
+# pull the latest version from repo
 sudo git pull origin master
 
 # Export dpkg files and repos
@@ -14,20 +23,27 @@ sudo apt-key exportall > ~/Repo.keys #creates ~/Repo.keys
 # Export python modules
 sudo -H pip2 install --upgrade pip
 sudo -H pip3 install --upgrade pip
-pip2 freeze > ~/pip2freeze.txt
-pip3 freeze > ~/pip3freeze.txt
+pip2 freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' > ~/pip2freeze.txt
+pip3 freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' > ~/pip3freeze.txt
+cat ~/pip2freeze.txt ~/pip3freeze.txt ~/pip37freeze.txt | sort > ~/pipfreeze.txt
+uniq ~/pipfreeze.txt > ~/pipfreezemerge.txt
+cut -d "=" -f1 ~/pipfreezemerge.txt | uniq > ~/pipfreezeinstall.txt
 
 # Export npm modules
 sudo npm update -g npm
 ls `npm root -g` > ~/npmmodules.txt
 
 # Push to GitHub
-cp -nRf ~/Package.list ./files
-cp -nRf ~/Repo.keys ./files
-cp -nRf ~/sources.list* ./files
-cp -nRf ~/pip2freeze.txt ./files
-cp -nRf ~/pip3freeze.txt ./files
-cp -nRf ~/npmmodules.txt ./files
+cd ~/GitHub/Linux/migrateinstall/ubuntu
+yes | /bin/cp -f ~/Package.list files
+yes | /bin/cp -f ~/Repo.keys files
+yes | /bin/cp -f ~/sources.list* files
+yes | /bin/cp -f ~/pip2freeze.txt files
+yes | /bin/cp -f ~/pip3freeze.txt files
+yes | /bin/cp -f ~/pipfreezemerge.txt files
+yes | /bin/cp -f ~/pipfreezeinstall.txt files
+yes | /bin/cp -f  ~/npmmodules.txt files
+git add -A
 git commit -am "Update ubuntu export files"
 git push #login to github
 
