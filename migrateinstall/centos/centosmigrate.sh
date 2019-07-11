ebc146977573a57c0a5f938fb08b1bc201b2b7f1
lpulle <laurencepulle@nel>
2019-07-11 13:43:35 +0100

    Update CentOS export files

--- migrateinstall/centos/centosmigrate.sh
+++ migrateinstall/centos/centosmigrate.sh
@@ -41,16 +41,19 @@ sudo -H /usr/local/bin/python3.7 -m pip install --upgrade pip
 #pip3 install -r ~/files/pip3freeze.txt
 
 #pip has a problem if pyOpenSSL and cryptography are old versions so uninstall and install latest versions
-sudo -H pip2 uninstall pyOpenSSL cryptography
-sudo -H pip2 install pyOpenSSL cryptography
-sudo -H pip3 uninstall pyOpenSSL cryptography
-sudo -H pip3 install pyOpenSSL cryptography
-sudo -H /usr/local/bin/pip3.7 uninstall pyOpenSSL cryptography
-sudo -H /usr/local/bin/pip3.7 install pyOpenSSL cryptography
-# install using a for loop so will keep going even with an error
-for i in $(cat ~/pipfreeze.txt); do sudo /usr/local/bin/pip3.7 install $i; done
-for i in $(cat ~/pipfreeze.txt); do sudo pip3 install $i; done
-for i in $(cat ~/pipfreeze.txt); do sudo pip2 install $i; done
+##fixed this in the export script now so no need to uninstall and reinstall now
+#sudo -H pip2 uninstall pyOpenSSL cryptography
+#sudo -H pip2 install pyOpenSSL cryptography
+#sudo -H pip3 uninstall pyOpenSSL cryptography
+#sudo -H pip3 install pyOpenSSL cryptography
+#sudo -H /usr/local/bin/pip3.7 uninstall pyOpenSSL cryptography
+#sudo -H /usr/local/bin/pip3.7 install pyOpenSSL cryptography
+
+# install using a for loop so will keep going even if it encounters an error
+# it will find errors as pipfreezeinstall.txt is a merge of python 2 and 3 module exports
+for i in $(cat ~/pipfreezeinstall.txt); do sudo /usr/local/bin/pip3.7 install $i; done
+for i in $(cat ~/pipfreezeinstall.txt); do sudo pip3 install $i; done
+for i in $(cat ~/pipfreezeinstall.txt); do sudo pip2 install $i; done
 
 # Install npm modules exported
 sed 's/#.*//' npmmodules.txt | xargs sudo npm install -g # this should work but stops if it gets an error
