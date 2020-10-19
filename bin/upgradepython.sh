8889aaaf3b66fc8567fea43941a57f3d3396b654
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2020-10-19 13:24:50 +0100

    Update upgradepython.sh
    pycairo and pyusb caused problem when updating - exclude them in the pip freeze commands
    They can be added back if this is ever resolved

--- bin/upgradepython.sh
+++ bin/upgradepython.sh
@@ -3,15 +3,15 @@
 sudo -H pip2 install --upgrade pip
 sudo -H pip2 install --upgrade setuptools pip
 sudo -H python -m pip install --upgrade pip
-sudo -H  pip2 install -U $(pip2 freeze | cut -d '=' -f 1)
+sudo -H  pip2 install -U $(pip2 freeze | cut -d '=' -f 1 | sed -e '/pycairo/d' | sed -e '/pyusb/d') #exclude pycairo and pyusb
 #3.6
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install --upgrade setuptools pip
 sudo -H python3.6 -m pip install --upgrade pip
-sudo -H pip3 install -U $(pip3 freeze | cut -d '=' -f 1)
+sudo -H pip3 install -U $(pip3 freeze | cut -d '=' -f 1 | sed -e '/pycairo/d' | sed -e '/pyusb/d') #exclude pycairo and pyusb
 #3.7
 sudo -H /usr/local/bin/pip3.7 install --upgrade pip
 sudo -H /usr/local/bin/pip3.7 install --upgrade setuptools pip
 sudo -H /usr/local/bin/python3.7 -m pip install --upgrade pip
 sudo -H pip install -U $(pip freeze | cut -d '=' -f 1)
-sudo -H /usr/local/bin/pip3.7 install -U $(/usr/local/bin/pip3.7 freeze | cut -d '=' -f 1)
+sudo -H /usr/local/bin/pip3.7 install -U $(/usr/local/bin/pip3.7 freeze | cut -d '=' -f 1 | sed -e '/pycairo/d' | sed -e '/pyusb/d') #exclude pycairo and pyusb
