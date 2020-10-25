bd49b20e119fc94227905c9d88429a666a365898
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2020-10-25 12:18:55 +0000

    Delete scrapersimple.ts

--- bin/scrapersimple.ts
+++
@@ -1,4 +0,0 @@
-// ES5 /TypeScript
-var getSubtitles = require('youtube-captions-scraper').getSubtitles;
-const args = require('yargs').argv;
-getSubtitles({videoID: args.id, lang: 'en'}).then(captions => {console.log(JSON.stringify(captions, null, 4));});
