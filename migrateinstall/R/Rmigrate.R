68090a2ea786a3b80ad72d45792f2adacab67e45
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2021-10-23 11:54:01 +0100

    Update Rmigrate.R
    x is a data frame with column Package - change install.packages to reflect this

--- migrateinstall/R/Rmigrate.R
+++ migrateinstall/R/Rmigrate.R
@@ -2,4 +2,4 @@ x <- read.csv("RPackages.csv", colClasses = "character")
 #for (i in (1:length(x[,])) ) { 
 #  if(!require(x[i])) install.packages(x[i,]) 
 #}
-install.packages(x)
+install.packages(x$Package)
