e9291c876797635aabc2b7f450c69bb64d8fbde4
root <root@LAPTOP-O2EVK1QH>
2022-09-13 20:20:36 +0100

    Update AlmaLinux export files

--- migrateinstall/AlmaMigrateFromCentOS/Postcentosexport.sh
+++ migrateinstall/AlmaMigrateFromCentOS/Postcentosexport.sh
@@ -4,71 +4,71 @@
 #sudo yum update
 
 # update Linux repo with latest files
-cd ~/GitHub/Linux
-DIRECTORY='~/GitHub/Linux'
+cd /home/pullel/GitHub/Linux
+DIRECTORY='/home/pullel/GitHub/Linux'
 if [ ! -d "$DIRECTORY" ]; then
   # If GitHub repo folder is missing clone it
-  cd ~/GitHub
+  cd /home/pullel/GitHub
   git clone https://github.com/LPulle/Linux
   cd Linux
   else
-   cd ~/GitHub/Linux 
+   cd /home/pullel/GitHub/Linux 
 fi
 # pull the latest version from repo
 sudo git pull origin master
 
 # Export rpm files and yum repos
-rpm -qa > ~/yum.installed
-sed -i 's/^/install /' ~/yum.installed
-echo run >> ~/yum.installed
-yes | sudo /bin/cp -nRf /etc/yum.repos.d/*.repo ~/ #creates ~/*.repo files
+rpm -qa > /home/pullel/yum.installed
+sed -i 's/^/install /' /home/pullel/yum.installed
+echo run >> /home/pullel/yum.installed
+yes | sudo /bin/cp -nRf /etc/yum.repos.d/*.repo /home/pullel #creates ~/*.repo files
 
 # Export rpm package names to installed-software.log
-rpm -qa --qf "%{NAME}\n" | sort | uniq > ~/GitHub/Linux/migrateinstall/AlmaMigrateFromCentOS/installed-software.log
+rpm -qa --qf "%{NAME}\n" | sort | uniq > /home/pullel/GitHub/Linux/migrateinstall/AlmaMigrateFromCentOS/installed-software.log
 
 # Upgrade pip and export all python modules (excluding pyOpenSSL and cryptography)
 sudo -H pip install --upgrade pip
 sudo -H pip3 install --upgrade pip
 #sudo -H /usr/local/bin/pip3.7 install --upgrade pip
-pip freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' | sed -e '/pycairo/d' | sed -e '/pyusb/d' > ~/pip2freeze.txt
-pip3 freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' | sed -e '/pycairo/d' | sed -e '/pyusb/d' > ~/pip3freeze.txt
+pip freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' | sed -e '/pycairo/d' | sed -e '/pyusb/d' > /home/pullel/pip2freeze.txt
+pip3 freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' | sed -e '/pycairo/d' | sed -e '/pyusb/d' > /home/pullel/pip3freeze.txt
 #/usr/local/bin/pip3.7 freeze | sed -e '/pyOpenSSL/d' | sed -e '/cryptography/d' | sed -e '/pycairo/d' | sed -e '/pyusb/d' > ~/pip37freeze.txt
-cat ~/pip2freeze.txt pip3freeze.txt | sort > ~/pipfreeze.txt
-uniq ~/pipfreeze.txt > ~/pipfreezemerge.txt
-cut -d "=" -f1 ~/pipfreezemerge.txt | uniq > ~/pipfreezeinstall.txt
+cat /home/pullel/pip2freeze.txt pip3freeze.txt | sort > /home/pullel/pipfreeze.txt
+uniq /home/pullel/pipfreeze.txt > /home/pullel/pipfreezemerge.txt
+cut -d "=" -f1 /home/pullel/pipfreezemerge.txt | uniq > /home/pullel/pipfreezeinstall.txt
 
 # Export npm modules
 sudo npm install -g npm@latest
 sudo npm install -g npm
 sudo npm update
 sudo npm update -g
-ls `npm root -g` > ~/npmmodules.txt
+ls `npm root -g` > /home/pullel/npmmodules.txt
 
 # Push to GitHub
-cd ~/GitHub/Linux/migrateinstall/AlmaMigrateFromCentOS
-yes | sudo /bin/cp -f ~/yum.installed files
-yes | sudo /bin/cp -f ~/pip2freeze.txt files
-yes | sudo /bin/cp -f ~/pip3freeze.txt files
-yes | sudo /bin/cp -f ~/pip37freeze.txt files
-yes | sudo /bin/cp -f ~/pipfreezemerge.txt files
-yes | sudo /bin/cp -f ~/pipfreezeinstall.txt files
-yes | sudo /bin/cp -f ~/npmmodules.txt files
-yes | sudo /bin/cp -f ~/*.repo files
+cd /home/pullel/GitHub/Linux/migrateinstall/AlmaMigrateFromCentOS
+yes | sudo /bin/cp -f /home/pullel/yum.installed files
+yes | sudo /bin/cp -f /home/pullel/pip2freeze.txt files
+yes | sudo /bin/cp -f /home/pullel/pip3freeze.txt files
+yes | sudo /bin/cp -f /home/pullel/pip37freeze.txt files
+yes | sudo /bin/cp -f /home/pullel/pipfreezemerge.txt files
+yes | sudo /bin/cp -f /home/pullel/pipfreezeinstall.txt files
+yes | sudo /bin/cp -f /home/pullel/npmmodules.txt files
+yes | sudo /bin/cp -f /home/pullel/*.repo files
 rm -f *.bak
 git add -A
 git commit -am "Update AlmaLinux export files"
 git push #login to github
 
 # Cleanup
-rm -f ~/yum.installed
-rm -f ~/pipfreeze.txt
-rm -f ~/pip2freeze.txt
-rm -f ~/pip3freeze.txt
-rm -f ~/pip37freeze.txt
-rm -f ~/pipfreezemerge.txt
-rm -f ~/pipfreezeinstall.txt
-rm -f ~/npmmodules.txt
-rm -f ~/*.repo
+rm -f /home/pullel/yum.installed
+rm -f /home/pullel/pipfreeze.txt
+rm -f /home/pullel/pip2freeze.txt
+rm -f /home/pullel/pip3freeze.txt
+rm -f /home/pullel/pip37freeze.txt
+rm -f /home/pullel/pipfreezemerge.txt
+rm -f /home/pullel/pipfreezeinstall.txt
+rm -f /home/pullel/npmmodules.txt
+rm -f /home/pullel/*.repo
 
 # Copy all files created from the above to new install into ~/files
 # Also copy across any dot files you may want
