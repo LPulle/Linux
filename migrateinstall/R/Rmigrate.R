870e4e94196d29ba6370fef3312d516ca7db5691
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2022-10-09 13:29:28 +0100

    Update Rmigrate.R

--- migrateinstall/R/Rmigrate.R
+++ migrateinstall/R/Rmigrate.R
@@ -1,13 +1,2 @@
-<<<<<<< HEAD
 x <- read.csv("RPackages.csv", colClasses = "character")
-#for (i in (1:length(x[,])) ) { 
-#  if(!require(x[i])) install.packages(x[i,]) 
-#}
-install.packages(x)
-=======
-x <- read.csv("RPackages.csv", colClasses = "character")
-#for (i in (1:length(x[,])) ) { 
-#  if(!require(x[i])) install.packages(x[i,]) 
-#}
 install.packages(x$Package)
->>>>>>> f7d1e5934faedf0c803c02f8085e20ecd5405195
