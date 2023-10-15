a649b0c45eb76123513576d9e4b7c9667fcb3695
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2023-10-15 11:16:51 +0100

    Update AlmaLinux export files

---
+++ migrateinstall/AlmaMigrateFromCentOS/install_repos.sh
@@ -0,0 +1,7 @@
+dnf config-manager --set-enabled crb
+dnf config-manager --set-enabled nfv
+dnf config-manager --set-enabled plus
+dnf config-manager --set-enabled resilientstorage
+dnf config-manager --set-enabled rt
+dnf config-manager --set-enabled sap
+dnf config-manager --set-enabled saphana
