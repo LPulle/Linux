636b813442b5f49ba227009163e9ce253fac974f
Laurence Pulle <45460723+LPulle@users.noreply.github.com>
2023-11-07 08:57:52 +0000

    Update install_repos_9.sh

--- migrateinstall/AlmaMigrateFromCentOS/install_repos_9.sh
+++ migrateinstall/AlmaMigrateFromCentOS/install_repos_9.sh
@@ -6,7 +6,7 @@ dnf config-manager --set-enabled rt
 dnf config-manager --set-enabled sap
 dnf config-manager --set-enabled saphana
 dnf config-manager --set-enabled epel-testing
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
 dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
 dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm -y
 dnf install http://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
@@ -16,6 +16,6 @@ rpm -Uvh https://packages.microsoft.com/config/rhel/9/packages-microsoft-prod.rp
 curl https://repo.almalinux.org/elevate/testing/elevate-testing.repo -o /etc/yum.repos.d/elevate-testing.repo
 rpm --import https://repo.almalinux.org/elevate/RPM-GPG-KEY-ELevate
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-dnf install https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm
+dnf install https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm -y
 dnf upgrade --refresh -y
 #dnf install almalinux-release-testing -y
